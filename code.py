6048868f24d3b910b901ccbd52ed354b37309987
harishchandhiran <61836574+harishchandhiran@users.noreply.github.com>
2021-07-25 13:13:56 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,8 @@
+text = input("Enter the text file name you want to split into lines: ")
+file = open(text)
+lines = []
+
+for i in file:
+    line = i.split(.)
+    lines.append(lines)
+print(lines)
